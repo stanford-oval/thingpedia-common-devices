63742658aa1ac691b44a1e516a8e82b107aaf49e
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-29 12:17:24 -0700

    Set access type for google devices
    To get a refresh token

--- com.gmail/device.js
+++ com.gmail/device.js
@@ -13,6 +13,7 @@ module.exports = new Tp.DeviceClass({
         scope: ['openid', 'profile', 'email', 'https://mail.google.com/'],
         authorize: 'https://accounts.google.com/o/oauth2/auth',
         get_access_token: 'https://www.googleapis.com/oauth2/v3/token',
+        set_access_type: true,
         callback: function(engine, accessToken, refreshToken) {
             var auth = 'Bearer ' + accessToken;
             return Tp.Helpers.Http.get('https://www.googleapis.com/oauth2/v2/userinfo', {auth: auth, accept: 'application/json'})
--- com.google.drive/device.js
+++ com.google.drive/device.js
@@ -12,15 +12,13 @@ module.exports = new Tp.DeviceClass({
     Name: 'GoogleAccountDevice',
     UseOAuth2: Tp.Helpers.OAuth2({
 		kind: 'com.google.drive',
-		client_id: '739906609557-o52ck15e1ge7deb8l0e80q92mpua1p55.apps.googleusercontent.com',
-		client_secret: 'drAqNZnVS_9jHl6KBENOPVXR',
 		scope: ['openid','profile','email',
-                'https://www.googleapis.com/auth/plus.me',
                 'https://www.googleapis.com/auth/drive',
                 'https://www.googleapis.com/auth/drive.appdata',
                 'https://www.googleapis.com/auth/drive.file'],
 		authorize: 'https://accounts.google.com/o/oauth2/auth',
 		get_access_token: 'https://www.googleapis.com/oauth2/v3/token',
+        set_access_type: true,
 		callback: function(engine, accessToken, refreshToken) {
 			var auth = 'Bearer ' + accessToken;
 			return Tp.Helpers.Http.get('https://www.googleapis.com/oauth2/v2/userinfo', { auth: auth, accept: 'application/json' })
