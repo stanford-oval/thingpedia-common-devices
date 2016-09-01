0297fa27dc2652b3306bee44ec099de5d884bdc8
Rakesh Ramesh <rakeshr1@stanford.edu>
2016-09-01 14:36:18 -0700

    Pushing phd comics

---
+++ com.phdcomics/new_post.js
@@ -0,0 +1,43 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// Copyright 2016 Shloka Desai <shloka@stanford.edu>
+//                Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See LICENSE for details
+
+const Tp = require('thingpedia');
+
+module.exports = new Tp.ChannelClass({
+    Name: 'GenericRSSPollingTrigger',
+    Extends: Tp.RSSPollingTrigger,
+    RequiredCapabilities: ['channel-state'],
+    interval: 3 * 3600 * 1000, // 3 hours
+
+    _init: function(engine, state, device) {
+        this.parent(engine, state, device);
+        this.url = this.device.url;
+    },
+
+    formatEvent(event) {
+        var title = event[0];
+        var link = event[1];
+	var img = event[2];
+
+        return [title, {
+            type: 'picture',
+	    url: img	
+        }];
+    },
+
+    _emit(entry) {
+        var title = entry[0];
+        var link = entry[1];
+        var xml = entry[2];
+        var updated = entry[4];
+	
+	var regex = /&lt;img.*src=&quot;([0-9a-zA-Z:\/\.]+)&quot;/g;
+	var img = regex.exec(xml)[0];
+
+        this.emitEvent([title, link, img]);
+    }
+});
