d3835fcf497a86081d776a61b441c13478126418
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-09-01 13:29:54 -0700

    Update generic rss to the new RSS API in Thingpedia

--- org.thingpedia.rss/new_post.js
+++ org.thingpedia.rss/new_post.js
@@ -6,19 +6,15 @@
 // See LICENSE for details
 
 const Tp = require('thingpedia');
-const xml2js = require('xml2js');
 
 module.exports = new Tp.ChannelClass({
-    Name: 'RSSPollingTrigger',
-    Extends: Tp.HttpPollingTrigger,
+    Name: 'GenericRSSPollingTrigger',
+    Extends: Tp.RSSPollingTrigger,
     RequiredCapabilities: ['channel-state'],
     interval: 3 * 3600 * 1000, // 3 hours
 
     _init: function(engine, state, device) {
-        this.parent();
-        this.device = device;
-        this._state = state;
-
+        this.parent(engine, state, device);
         this.url = this.device.url;
     },
 
@@ -35,50 +31,12 @@ module.exports = new Tp.ChannelClass({
         }];
     },
 
-    _onResponse(response) {
-        var state = this._state;
-        var lastRead = state.get('last-read');
+    _emit(entry) {
+        var title = entry[0];
+        var link = entry[1];
+        var xml = entry[2];
+        var updated = entry[4];
 
-        return new Promise((callback, errback) => {
-            xml2js.parseString(response, (err, res) => {
-                if (err)
-                    errback(err);
-                else
-                    callback(res);
-            });
-        }).then((parsed) => {
-            var newest = undefined;
-
-            var toEmit = [];
-            if (parsed.feed) {
-                for (var entry of parsed.feed.entry) {
-                    var updated = +new Date(entry.updated[0]);
-                    if (newest === undefined ||
-                        newest < updated)
-                        newest = updated;
-                    if (updated <= lastRead)
-                        continue;
-                    toEmit.push([entry.title[0], entry.link[0].$.href, updated]);
-                }
-            } else {
-                for (var entry of parsed.rss.channel[0].item) {
-                    var updated = +new Date(entry.pubDate[0]);
-                    if (newest === undefined ||
-                        newest < updated)
-                        newest = updated;
-                    if (updated <= lastRead)
-                        continue;
-                    toEmit.push([entry.title[0], entry.link[0], updated]);
-                }
-            }
-
-            toEmit.sort(function(a, b) {
-                return a[2] - b[2];
-            });
-
-            state.set('last-read', newest);
-            for (var entry of toEmit)
-                this.emitEvent([entry[0], entry[1]]);
-        });
+        this.emitEvent([title, link]);
     }
 });
