abc4c64400e316fa2353f86b7401c7e80254afb0
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-12-14 17:07:58 -0800

    Update thecatapi

--- com.thecatapi/device.js
+++ com.thecatapi/device.js
@@ -4,17 +4,31 @@
 //                Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See LICENSE for details
+"use strict";
 
 const Tp = require('thingpedia');
 
-module.exports = new Tp.DeviceClass({
-    Name: "CatAPIDevice",
+const URL = 'http://thecatapi.com/api/images/get?api_key=MTAxNzA1&format=xml&type=jpg,png';
 
-    _init: function(engine, state) {
-        this.parent(engine, state);
+module.exports = class CatAPIDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+        super(engine, state);
 
         this.uniqueId = 'com.thecatapi';
         this.name = "The Cat API";
         this.description = "Where every day is Caturday!";
     }
-});
+
+    get_get({ count }) {
+        // fetch 1 cat by default
+        count = count || 1;
+        const url = URL + '&results_per_page=' + count;
+        return Tp.Helpers.Http.get(url).then((result) => Tp.Helpers.Xml.parseString(result))
+        .then((parsed) => {
+            const array = parsed.response.data[0].images[0].image;
+            return array.map((image) => {
+                return { image_id: image.id[0], count: array.length, link: image.url[0], picture_url: image.source_url[0] };
+            });
+        });
+    }
+};
