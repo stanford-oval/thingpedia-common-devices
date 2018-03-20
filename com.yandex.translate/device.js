96fa0085c079c59c6575ad5221be6032a903c924
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-20 11:15:37 -0700

    Update yandex to v2

--- com.yandex.translate/device.js
+++ com.yandex.translate/device.js
@@ -2,23 +2,56 @@
 //
 // Copyright 2016 Vivek Jain <vsjain@stanford.edu>
 //                James Hong <jamesh93@stanford.edu>
-//                Giovanni Campagna <gcampagn@cs.stanford.edu>
+//           2016-2018 Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See LICENSE for details
+"use strict";
 
 const Tp = require('thingpedia');
 
-const API_KEY = 'trnsl.1.1.20160303T103221Z.a4b21d757023b0ee.8fcdf6451b8964e5a64e3bf9ca1a9797ba355924';
+const languages = require('./languages.json');
 
-module.exports = new Tp.DeviceClass({
-    Name: 'YandexTranslateDevice',
-
-    _init: function(engine, state) {
-        this.parent(engine, state);
+module.exports = class YandexTranslateDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+        super(engine, state);
 
         this.uniqueId = 'com.yandex.translate';
         this.name = "Yandex Translate";
         this.description = "Translate using Yandex.";
-        this.apiKey = API_KEY;
+        this._apiKey = this.constructor.metadata.auth.api_key;
     }
-});
+
+    get_translate({ source_language, target_language, text }) {
+        if (source_language === undefined || source_language === null)
+            source_language = '';
+        else
+            source_language = String(source_language);
+        target_language = String(target_language);
+
+        const langPair = source_language ? (source_language + '-' + target_language) : target_language;
+        const url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + this._apiKey +
+            "&text=" + encodeURIComponent(text) + "&lang=" + langPair;
+
+        return Tp.Helpers.Http.get(url).then((response) => {
+            const parsed = JSON.parse(response);
+            if (parsed.code !== 200)
+                throw new Error('Translation error: ' + response);
+
+            return [{ translated_text: parsed.text[0] }];
+        });
+    }
+
+    get_detect_language({ text }) {
+        const url = "https://translate.yandex.net/api/v1.5/tr.json/detect?key="
+            + this._apiKey + "&text=" + encodeURIComponent(text);
+
+        return Tp.Helpers.Http.get(url).then((response) => {
+            console.log(response);
+            const parsed = JSON.parse(response);
+            if (parsed.code !== 200)
+                throw new Error('Failed to detect language');
+
+            return [{ value: new Tp.Value.Entity(parsed.lang, languages[parsed.lang]) }];
+        });
+    }
+};
