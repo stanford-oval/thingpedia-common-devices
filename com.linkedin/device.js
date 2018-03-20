76c36b77085775a6f9a2da5a6c3c577f0b8258f6
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-19 21:12:07 -0700

    Update com.linkedin to v2

--- com.linkedin/device.js
+++ com.linkedin/device.js
@@ -3,23 +3,26 @@
 // Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See LICENSE for details
+"use strict";
 
 const Tp = require('thingpedia');
 
-module.exports = new Tp.DeviceClass({
-    Name: 'LinkedinDevice',
-    UseOAuth2: Tp.Helpers.OAuth2({
-        authorize: 'https://www.linkedin.com/uas/oauth2/authorization',
-        get_access_token: 'https://www.linkedin.com/uas/oauth2/accessToken',
-        set_state: true,
+const PROFILE_URL = 'https://api.linkedin.com/v1/people/~:(id,formatted-name,headline,industry,specialties,positions,picture-url)?format=json';
+const SHARE_URL = 'https://api.linkedin.com/v1/people/~/shares?format=json';
 
-        callback: function(engine, accessToken, refreshToken) {
-            var auth = 'Bearer ' + accessToken;
-            return Tp.Helpers.Http.get('https://api.linkedin.com/v1/people/~:(id,formatted-name)?format=json',
-                                       { auth: auth,
-                                         accept: 'application/json' })
-                .then(function(response) {
-                    var parsed = JSON.parse(response);
+module.exports = class LinkedinDevice extends Tp.BaseDevice {
+    static get runOAuth2() {
+        return Tp.Helpers.OAuth2({
+            authorize: 'https://www.linkedin.com/uas/oauth2/authorization',
+            get_access_token: 'https://www.linkedin.com/uas/oauth2/accessToken',
+            set_state: true,
+
+            callback(engine, accessToken, refreshToken) {
+                const auth = 'Bearer ' + accessToken;
+                return Tp.Helpers.Http.get('https://api.linkedin.com/v1/people/~:(id,formatted-name)?format=json',
+                                           { auth: auth,
+                                             accept: 'application/json' }).then((response) => {
+                    const parsed = JSON.parse(response);
                     return engine.devices.loadOneDevice({ kind: 'com.linkedin',
                                                           accessToken: accessToken,
                                                           refreshToken: refreshToken,
@@ -27,27 +30,56 @@ module.exports = new Tp.DeviceClass({
                                                           userName: parsed.formattedName
                                                         }, true);
                 });
-        }
-    }),
+            }
+        });
+    }
 
-    _init: function(engine, state) {
-        this.parent(engine, state);
+    constructor(engine, state) {
+        super(engine, state);
 
         this.uniqueId = 'com.linkedin-' + this.userId;
         this.name = "LinkedIn Account of %s".format(this.userName);
         this.description = "This is your LinkedIn account";
-    },
+    }
 
     get userId() {
         return this.state.userId;
-    },
+    }
 
     get userName() {
         return this.state.userName;
-    },
+    }
 
-    checkAvailable: function() {
+    checkAvailable() {
         return Tp.Availability.AVAILABLE;
     }
-});
+
+    get_get_profile() {
+        return Tp.Helpers.Http.get(PROFILE_URL, {
+            useOAuth2: this,
+            accept: 'application/json' }).then((response) => {
+            const parsed = JSON.parse(response);
+
+            return [{ formatted_name: parsed.formattedName,
+                      headline: parsed.headline || '',
+                      industry: parsed.industry || '',
+                      specialties: parsed.specialties || '',
+                      positions: ('values' in parsed) ? parsed.positions.values.map((p) => p.summary) : [],
+                      profile_picture: parsed.pictureUrl || '' }];
+        });
+    }
+
+    do_share({ status }) {
+        return Tp.Helpers.Http.post(SHARE_URL, JSON.stringify({
+            comment: status,
+            visibility: {
+                code: 'anyone'
+            }
+        }), {
+            useOAuth2: this,
+            dataContentType: 'application/json',
+            accept: 'application/json'
+        });
+    }
+};
 
