9e5fc380720c59b77a658b953002bedbb8082a42
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-20 10:41:31 -0700

    Port Reddit and Washington Post to v2

--- com.reddit.frontpage/device.js
+++ com.reddit.frontpage/device.js
@@ -1,21 +1,55 @@
 // -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
 //
 // Copyright 2016 Jia-Han Chiam <jiahan@stanford.edu>
-//                Giovanni Campagna <gcampagn@cs.stanford.edu>
+//           2016-2018 Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See LICENSE for details
+"use strict";
 
 const Tp = require('thingpedia');
 
-module.exports = new Tp.DeviceClass({
-    Name: "RedditFrontpageDevice",
-
-    _init(engine, state) {
-         this.parent(engine, state);
+module.exports = class RedditFrontpageDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+         super(engine, state);
 
          this.uniqueId = "com.reddit.frontpage";
 
          this.name = "Reddit Front Page";
          this.description = "Reddit Front Page watches for posts that reach the front page on Reddit.";
     }
-});
+
+    get_get(params, filter) {
+        let url;
+
+        /* FIXME filters
+        if (params[2] !== null && params[2] !== undefined) {
+            url = 'https://www.reddit.com' + params[2] + '/.rss';
+            this.filterString = 'user-' + params[2];
+        } else if (params[3] !== null && params[3] !== undefined) {
+            url = 'https://www.reddit.com' + params[3] + '/.rss';
+            this.filterString = 'category-' + params[3];
+        } else {
+            // #nofilter
+            url = 'https://www.reddit.com/.rss';
+        }*/
+
+        url = 'https://www.reddit.com/.rss';
+
+        return Tp.Helpers.Http.get(url).then(Tp.Helpers.Xml.parseString).then((parsed) => {
+            let result = [];
+            for (let entry of parsed.feed.entry) {
+                let updated = new Date(entry.updated[0]);
+                let title = entry.title[0]._ || entry.title[0];
+                let link = entry.link[0].$.href;
+                let user = entry.author[0].name[0];
+                if (user.startsWith('/u/'))
+                    user = user.substring(3);
+                let category = entry.category[0].$.term;
+                result.push({ title, link, updated, user, category });
+            }
+
+            result.sort((a, b) => (+b.updated_time) - (+a.updated_time));
+            return result;
+        });
+    }
+};
--- com.washingtonpost/device.js
+++ com.washingtonpost/device.js
@@ -1,20 +1,63 @@
 // -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
 //
 // Copyright 2016 Shloka Desai <shloka@stanford.edu>
-//                Giovanni Campagna <gcampagn@cs.stanford.edu>
+//           2016-2018 Giovanni Campagna <gcampagn@cs.stanford.edu>
 //
 // See LICENSE for details
+"use strict";
 
 const Tp = require('thingpedia');
 
-module.exports = new Tp.DeviceClass({
-    Name: "RSSDevice",
+const BLOGS = {
+    the_fix: 'http://feeds.washingtonpost.com/rss/rss_the-fix',
+    politics: 'http://feeds.washingtonpost.com/rss/rss_election-2012',
+    powerpost: 'http://feeds.washingtonpost.com/rss/rss_powerpost',
+    fact_checker: 'http://feeds.washingtonpost.com/rss/rss_fact-checker',
+    world_views: 'http://feeds.washingtonpost.com/rss/rss_blogpost',
+    compost: 'http://feeds.washingtonpost.com/rss/rss_compost',
+    the_plum_line: 'http://feeds.washingtonpost.com/rss/rss_plum-line',
+    post_partisan: 'http://feeds.washingtonpost.com/rss/rss_post-partisan',
+    post_everything: 'http://feeds.washingtonpost.com/rss/rss_post-everything',
+    right_turn: 'http://feeds.washingtonpost.com/rss/rss_right-turn',
+    capital_weather_gang: 'http://feeds.washingtonpost.com/rss/rss_capital-weather-gang',
+    morning_mix: 'http://feeds.washingtonpost.com/rss/rss_morning-mix',
+    wonkblog: 'http://feeds.washingtonpost.com/rss/rss_wonkblog'
+};
 
-    _init: function(engine, state) {
-         this.parent(engine, state);
+module.exports = class WashingtonPostDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+         super(engine, state);
 
          this.uniqueId = 'com.washingtonpost';
          this.name = "The Washington Post";
          this.description = "Breaking News, World, US, DC News & Analysis";
-    },
-});
+    }
+
+    get_get_article({ section }) {
+        const url = 'http://feeds.washingtonpost.com/rss/' + section;
+
+        // Unfortunately, the feeds above do not have pubDate set properly for the articles,
+        // so we cannot use the generic RSS code
+
+        return Tp.Helpers.Http.get(url).then(Tp.Helpers.Xml.parseString).then((parsed) => {
+            const result = [];
+            for (var entry of parsed.rss.channel[0].item) {
+                result.push({
+                    title: entry.title[0],
+                    link: entry.link[0],
+                    description: entry.description ? entry.description[0] : '',
+                    picture_url: entry['media:group'] && entry['media:group'][0]['media:content'] ? entry['media:group'][0]['media:content'][2].$.url : ''
+                });
+            }
+            return result;
+        });
+    }
+
+    get_get_blog_post({ section }) {
+        const url = BLOGS[section];
+
+        // Unlike newspaper sections, the blog use more or less standard wordpress and
+        // implement rss properly, so we can just leave it to the generic code
+        return Tp.Helpers.Rss.get(url);
+    }
+};
