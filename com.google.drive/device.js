39b98172a11ba6a3416e1dade9566a547f599ba2
Silei Xu <silei@cs.stanford.edu>
2018-07-18 10:04:54 -0700

    fix google drive

--- com.google.drive/device.js
+++ com.google.drive/device.js
@@ -14,7 +14,7 @@ const Tp = require('thingpedia');
 const PAGE_SIZE = 10;
 
 module.exports = class GoogleDriveDevice extends Tp.BaseDevice {
-    static get runOAuth() {
+    static get runOAuth2() {
         return Tp.Helpers.OAuth2({
             kind: 'com.google.drive',
             scope: ['openid','profile','email',
@@ -29,10 +29,11 @@ module.exports = class GoogleDriveDevice extends Tp.BaseDevice {
                 return Tp.Helpers.Http.get('https://www.googleapis.com/oauth2/v2/userinfo', { auth: auth, accept: 'application/json' })
                 .then((response) => {
                     var parsed = JSON.parse(response);
-                    return engine.devices.loadOneDevice({ kind: 'com.google',
+                    return engine.devices.loadOneDevice({ kind: 'com.google.drive',
                                                           accessToken: accessToken,
                                                           refreshToken: refreshToken,
-                                                          profileId: parsed.id }, true);
+                                                          profileId: parsed.id,
+                                                          userName: parsed.name }, true);
                 });
             }
         });
@@ -45,12 +46,16 @@ module.exports = class GoogleDriveDevice extends Tp.BaseDevice {
         // hope
         // please do not follow this example
         this.uniqueId = 'google-drive-' + this.profileId;
-        this.name = "Google Drive Account %s".format(this.profileId);
+        this.name = "Google Drive Account %s".format(this.userName);
         this.description = "This is your Google Drive Account. You can use it to access and manage files on Google Drive.";
     }
 
     get profileId() {
         return this.state.profileId;
+    }
+
+    get userName() {
+        return this.state.userName;
     }
 
     do_create_new_drive_file({ file_name }) {
@@ -60,20 +65,28 @@ module.exports = class GoogleDriveDevice extends Tp.BaseDevice {
         return Tp.Helpers.Http.post(url, data, { useOAuth2: this, dataContentType: 'application/json' });
     }
 
-    get_drive_file_list({ order_by }) {
+    get_list_drive_files({ order_by }) {
         const ORDER_CLAUSES = {
-            'modified_time_decreasing': 'modifiedDate desc',
-            'modified_time_increasing': 'modifiedDate asc',
-            'created_time_decreasing': 'createdDate desc',
-            'created_time_increasing': 'createdDate asc',
+            'modified_time_decreasing': 'modifiedTime desc',
+            'modified_time_increasing': 'modifiedTime asc',
+            'created_time_decreasing': 'createdTime desc',
+            'created_time_increasing': 'createdTime asc',
             'name_decreasing': 'name desc',
             'name_increasing': 'name asc',
         };
-
-        const url = 'https://www.googleapis.com/drive/v3/files?orderBy=' + ORDER_CLAUSES[order_by || 'modified_time_decreasing'] + '&pageSize=' + PAGE_SIZE;
+        const order = ORDER_CLAUSES[order_by || 'modified_time_decreasing'];
+        const url = `https://www.googleapis.com/drive/v3/files?orderBy=${order}&pageSize=${PAGE_SIZE}&fields=*`;
         return Tp.Helpers.Http.get(url, { useOAuth2: this, dataContentType: 'application/json' }).then((response) => {
             const parsedResponse = JSON.parse(response);
             return parsedResponse.files.map((file) => {
+                console.log(JSON.stringify(file));
+                let last_modified_by = file.lastModifyingUser;
+                if (last_modified_by)
+                    last_modified_by = last_modified_by.displayName || last_modified_by.emailAddress || 'anonymous user';
+                else if (file.owners && file.owners.length === 1)
+                    last_modified_by = file.owners[0].displayName || file.owners[0].emailAddress || 'unknown user';
+                else
+                    last_modified_by = 'unknown user';
                 return ({
                     file_id: file.id,
                     file_name: file.name,
@@ -82,7 +95,9 @@ module.exports = class GoogleDriveDevice extends Tp.BaseDevice {
                     starred: file.starred,
                     created_time: new Date(file.createdTime),
                     modified_time: new Date(file.modifiedTime),
-                    size: file.size||0
+                    last_modified_by: last_modified_by,
+                    file_size: file.size||0,
+                    link: file.webViewLink
                 });
             });
         });
