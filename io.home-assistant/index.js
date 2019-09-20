6f3e223d7650aa5ca0b497f17019cff03c25edac
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-09-20 10:56:53 -0700

    Added io.home-assistant

---
+++ io.home-assistant/index.js
@@ -0,0 +1,13 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of io.home-assistant
+//
+// Copyright 2019 Giovanni Campagna <gcampagn@cs.stanford.edu>,Silei Xu <silei@cs.stanford.edu>
+//
+// See LICENSE for details
+"use strict";
+
+const Tp = require('thingpedia');
+
+module.exports = class extends Tp.BaseDevice {
+};
