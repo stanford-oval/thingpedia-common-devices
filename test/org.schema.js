57a1bd30f18455376d3d004264cc5d8f5e719798
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-03 20:38:04 -0700

    Added org.schema

---
+++ test/org.schema.js
@@ -0,0 +1,9 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// Copyright 2019 Giovanni Campagna <gcampagn@cs.stanford.edu>,Silei Xu <silei@cs.stanford.edu>
+//
+// See LICENSE for details
+"use strict";
+
+module.exports = [
+];
