54a089c712fbf79e589b400b751e7d4e3063704b
Ryan Cheng <ryachen@nuevaschool.org>
2019-11-06 10:29:18 -0800

    Revert "added unit test"
    This reverts commit 596e2d180890d64841e72e41eae67d1bfa6926df.

--- test/index.js
+++ test/index.js
@@ -60,15 +60,15 @@ async function runTest(instance, test) {
     let [testType, functionName, input, expected] = test;
 
     switch (testType) {
-        case 'query':
-            await testQuery(instance, functionName, input, expected);
-            break;
-        case 'monitor':
-            // do something
-            break;
-        case 'action':
-            // do something
-            break;
+    case 'query':
+        await testQuery(instance, functionName, input, expected);
+        break;
+    case 'monitor':
+        // do something
+        break;
+    case 'action':
+        // do something
+        break;
     }
 }
 
@@ -82,7 +82,7 @@ async function testOne(deviceKind) {
     let testsuite;
     try {
         testsuite = require('./' + deviceKind);
-    } catch (e) {
+    } catch(e) {
         console.log('No tests found for ' + deviceKind);
         // exit with no error and without loading the device
         // class (which would pollute code coverage statistics)
@@ -90,7 +90,6 @@ async function testOne(deviceKind) {
     }
 
     // require the device once fully (to get complete code coverage)
-    console.log('../' + deviceKind);
     require('../' + deviceKind);
 
     // now load the device through the TpClient loader code
@@ -131,7 +130,7 @@ async function testOne(deviceKind) {
         const test = testsuite[i];
         try {
             await runTest(instance, test);
-        } catch (e) {
+        } catch(e) {
             console.log('## FAILED: ' + e.message);
             console.log(e.stack);
             _anyFailed = true;
@@ -144,7 +143,7 @@ async function existsSafe(path) {
     try {
         await util.promisify(fs.access)(path);
         return true;
-    } catch (e) {
+    } catch(e) {
         if (e.code === 'ENOENT')
             return false;
         if (e.code === 'ENOTDIR')
@@ -156,10 +155,9 @@ async function existsSafe(path) {
 async function main() {
     if (process.argv.length > 2) {
         for (let toTest of process.argv.slice(2))
-            await testOne(toTest);
+             await testOne(toTest);
     } else {
-        for (let name of await util.promisify(fs.readdir)(path.resolve(path.dirname(module.filename),
-                '..'))) {
+        for (let name of await util.promisify(fs.readdir)(path.resolve(path.dirname(module.filename), '..'))) {
             if (!await existsSafe(name + '/manifest.tt')) //'
                 continue;
 
