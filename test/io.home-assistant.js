e9263dafa13ebfd3039114f821c38b8fa7a5710f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-11-05 18:49:18 -0800

    Remove home assistant unit tests
    We don't have an Home Assistant in the CI, and all they do is
    fail the CI, which is annoying.

--- test/io.home-assistant.js
+++
@@ -1,9 +0,0 @@
-// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
-//
-// Copyright 2019 Giovanni Campagna <gcampagn@cs.stanford.edu>,Silei Xu <silei@cs.stanford.edu>
-//
-// See LICENSE for details
-"use strict";
-
-module.exports = [
-];
