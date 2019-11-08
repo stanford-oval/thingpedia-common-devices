1a8b79575ff64c4b99581c876a9b836fbc23f332
Ryan Cheng <ryachen@nuevaschool.org>
2019-11-08 11:25:01 -0800

    fixed test

--- test/com.medium.js
+++ test/com.medium.js
@@ -5,9 +5,10 @@ const assert = require('assert');
 module.exports = [
     ['query', 'articles', { author: 'medium' }, (results) => {
         for (let result of results) {
-            assert(result.title === null || typeof result.title === 'string');
+
+            assert(result.title[0] === null || typeof result.title[0] === 'string');
             assert(result.link === null || result.link.startsWith('http'));
-            assert(result.updated instanceof Date);
+            assert(new Date(result.updated[0]) instanceof Date);
         }
-    }]
+        }]
 ];
