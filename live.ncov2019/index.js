c402eaed3b655fdfb798eb60a49b68a6fcb06712
Silei <silei@stanford.edu>
2020-02-29 17:16:15 -0800

    Add device to track coronavirus stats

---
+++ live.ncov2019/index.js
@@ -0,0 +1,83 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// Copyright 2020 Silei Xu <silei@cs.stanford.edu>
+//
+// See LICENSE for details
+"use strict";
+
+const Tp = require('thingpedia');
+const url = 'https://ncov2019.live/';
+const cheerio = require('cheerio');
+const { overwrite, getCode } = require('country-list');
+
+overwrite([
+    { code: 'us', name: 'United States' },
+    { code: 'tw', name: 'Taiwan' },
+    { code: 'gb', name: 'United Kingdom'},
+    { code: 'kr', name: 'South Korea' },
+    { code: 'ir', name: 'Iran' },
+    { code: 'ir', name: 'Vietnam' },
+    { code: 'mo', name: 'Macau' },
+    { code: 'ru', name: 'Russia' },
+    { code: 'ae', name: 'UAE'}
+]);
+
+module.exports = class NCov2019 extends Tp.BaseDevice {
+    get_stats() {
+        return Tp.Helpers.Http.get(url).then((res) => {
+            const result = [];
+            const $ = cheerio.load(res);
+
+            // load data outside china
+            const international = $('p:contains("International")').next();
+            let deathOutsideChina = 0;
+            international.find('td').each((i, td) => {
+                let children = $(td).contents();
+                let previous = null;
+                let data = {};
+                children.each((i, child) => {
+                    let current = $(child).text().trim();
+                    if (previous === null) {
+                        let country_code = getCode(current);
+                        if (country_code)
+                            data.country = { value: country_code.toLowerCase(), display: current };
+                    } else if (previous === 'Confirmed:') {
+                        data.confirmed = parseInt(current.replace(/,/g, ''));
+                    } else if (previous === 'Dead:') {
+                        data.death = parseInt(current.replace(/,/g, ''));
+                        deathOutsideChina += data.death;
+                    }
+                    previous = current;
+                });
+                if (data.country) {
+                    if (!('confirmed' in data))
+                        data.confirmed = 0;
+                    if (!('death' in data))
+                        data.death = 0;
+                    result.push(data);
+                }
+            });
+
+            const totalDeath = parseInt($('.fa-skull-crossbones').parent().contents()[2].data.replace(/,/g, ''));
+            const death = totalDeath - deathOutsideChina;
+
+            // load data from china
+            const china = $('p:contains("Mainland China")').next();
+            let confirmed = 0;
+            china.find('td').each((i, td) => {
+                let children = $(td).contents();
+                let previous = null;
+                children.each((i, child) => {
+                    let current = $(child).text().trim();
+                    if (previous === 'Confirmed:')
+                        confirmed += parseInt(current.replace(/,/g, ''));
+                    previous = current;
+                });
+            });
+            result.unshift({ country: { value: 'cn', display: 'China' } , confirmed, death });
+
+
+            return result;
+        });
+    }
+};
