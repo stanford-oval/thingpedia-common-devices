0d4a9107a44c71557db4f7af6f20b83c16d0e316
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-23 15:14:37 -0700

    Add script to deploy the model
    To the staging server only (to prod, we obviously need something
    less taped together...)

---
+++ scripts/deploy-model.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+model="$1"
+release=universe
+
+make "release=${release}" "eval/${release}/models/${model}/best.pth"
+
+rm -fr export/
+genienlp export --path "eval/${release}/models/${model}/" -o export/
+
+version=2
+AWS_PROFILE=oval aws s3 sync export/ "s3://almond-training/staging/inference/org.thingpedia.models.contextual:en-v${version}/"
+
+set +x
+ADMIN_TOKEN=$(secret-tool lookup almond.nlp_admin_token dev)
+curl -v -d '' "https://nlp-staging.almond.stanford.edu/admin/reload/@org.thingpedia.models.contextual/en?admin_token=${ADMIN_TOKEN}"
