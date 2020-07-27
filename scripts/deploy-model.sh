ff90d5a05caf0d7543d85ac1b100b63d9cce14a2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-27 11:16:37 -0700

    Update script to deploy model

--- scripts/deploy-model.sh
+++ scripts/deploy-model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
-set -x
-
 srcdir=`dirname $0`
 . $srcdir/lib.sh
 parse_args "$0" "release model" "$@"
 shift $n
+
+set -e
+set -x
 
 make "release=${release}" "eval/${release}/models/${model}/best.pth"
 
@@ -14,8 +14,17 @@ rm -fr export/
 genienlp export --path "eval/${release}/models/${model}/" -o export/
 
 version=2
-AWS_PROFILE=oval aws s3 sync export/ "s3://almond-training/staging/inference/org.thingpedia.models.contextual:en-v${version}/"
+if test "${release}" = "main"; then
+	model_tag="org.thingpedia.models.contextual"
+elif test "${release}" = "universe" ; then
+	model_tag="org.thingpedia.models.developer.contextual"
+else
+	echo "Invalid release (staging models cannot be deployed)"
+fi
+
+AWS_PROFILE=oval aws s3 sync export/ "s3://almond-training/staging/inference/${model_tag}:en-v${version}/"
 
 set +x
 ADMIN_TOKEN=$(secret-tool lookup almond.nlp_admin_token dev)
-curl -v -d '' "https://nlp-staging.almond.stanford.edu/admin/reload/@org.thingpedia.models.contextual/en?admin_token=${ADMIN_TOKEN}"
+curl -v -d '' "https://nlp-staging.almond.stanford.edu/admin/reload/@${model_tag}/en?admin_token=${ADMIN_TOKEN}"
+echo
