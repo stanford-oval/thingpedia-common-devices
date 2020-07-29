a58c7e45e9b6eb2a15a37d8b08ea97b3fb8a1b4d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-29 14:38:35 -0700

    Add script to find duplicates in a dataset
    Duplicates here means identical sentences in the same context
    that map to different programs. This script is a diagnostic to
    find bugs in the annotations or in the templates.

---
+++ scripts/find-duplicates.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+from collections import defaultdict
+
+def main():
+    alltargets = defaultdict(set)
+
+    for line in sys.stdin:
+        _id, context, sentence, target = line.strip().split('\t')
+
+        key = (context, sentence)
+        alltargets[key].add(target)
+
+    for key, targets in alltargets.items():
+        if len(targets) <= 1:
+            continue
+        print()
+        print(key[-1], sep='\t')
+        for target in targets:
+            print(target)
+
+main()
