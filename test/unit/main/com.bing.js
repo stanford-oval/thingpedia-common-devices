e6979d1351e1a9e0a7b6df7f01dcf02add630701
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-12-31 16:41:48 -0800

    Restore Bing
    Web and image search are important enough as a general fallback
    that I'm promoting them to "main" quality.

--- test/unit/main/com.bing.js
+++ test/unit/main/com.bing.js
@@ -8,11 +8,11 @@
 const assert = require('assert');
 
 module.exports = [
-    ['query', 'web_search', { query: 'pizza' }, (results) => {
+    ['query', 'web_search', { query: 'pizza' }, {}, (results) => {
         for (let result of results) {
             assert.strictEqual(typeof result.title, 'string');
             assert.strictEqual(typeof result.description, 'string');
-            assert(result.link.startsWith('http'));
+            assert(String(result.link).startsWith('http'));
 
             // either the title or the description should have pizza
             // somewhere
@@ -21,10 +21,10 @@ module.exports = [
         }
     }],
 
-    ['query', 'image_search', { query: 'almond' }, (results) => {
+    ['query', 'image_search', { query: 'almond' }, {}, (results) => {
         for (let result of results) {
             assert.strictEqual(typeof result.title, 'string');
-            assert(result.link.startsWith('http'));
+            assert(String(result.link).startsWith('http'));
             assert(result.picture_url.startsWith('http'));
             assert.strictEqual(typeof result.width, 'number');
             assert.strictEqual(typeof result.height, 'number');
