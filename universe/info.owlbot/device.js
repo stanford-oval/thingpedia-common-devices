059589b574e0c7771cd35c38a4e4559078d4c0ef
AydanPirani <aydanpirani@gmail.com>
2021-01-18 14:17:06 -0800

    Resolved synctactical changes.

--- universe/info.owlbot/device.js
+++ universe/info.owlbot/device.js
@@ -7,13 +7,11 @@ const Owlbot = require('owlbot-js');
 module.exports = class DictionaryAPIDevice extends Tp.BaseDevice {
 
   async get_get({word}) {
-    var key = this.constructor.metadata.auth.api_key;
-    var client = Owlbot(key);
+    let key = this.constructor.metadata.auth.api_key;
+    let client = Owlbot(key);
 
     return client.define(word).then(function(result){
-      console.log(result);
       let temp =  result["definitions"][0]["definition"];
-      console.log(temp);
       return [{"definition": temp}];
     });
   }
