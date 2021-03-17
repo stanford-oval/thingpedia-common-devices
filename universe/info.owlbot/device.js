38297d1d2d9c7c886f6bc5d18327fe1dc6c3c5a5
AydanPirani <aydanpirani@gmail.com>
2021-03-17 16:41:39 -0700

    Fixes

--- universe/info.owlbot/device.js
+++ universe/info.owlbot/device.js
@@ -42,6 +42,17 @@ module.exports = class DictionaryAPIDevice extends Tp.BaseDevice {
     return client.define(word).then((result) => {
       let temp =  result["definitions"][0]["definition"];
       return [{"definition": temp}];
+    }).catch((e) => {
+      if (e.response.statusText == "Not Found") {
+        console.log("Throwing!")
+        const error = new Error();
+        error.code = "unknown_definition";
+        throw error;
+      } else {
+        const error = new Error();
+        error.code = "other_error";
+        throw error;
+      }
     });
   }
 };
