54cfa29fca59fc631dabd0751f69088cf58e176f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-03-23 20:40:54 -0700

    Update deploy-model script, make it useful again
    We don't need this script to deploy the model, but we can use it
    to package the model for others to download.

--- scripts/deploy-model.sh
+++ scripts/deploy-model.sh
@@ -2,7 +2,7 @@
 
 srcdir=`dirname $0`
 . $srcdir/lib.sh
-parse_args "$0" "release=universe model" "$@"
+parse_args "$0" "model" "$@"
 shift $n
 
 set -e
@@ -12,15 +12,6 @@ aws s3 sync --exclude '*/dataset/*' --exclude '*/cache/*' --exclude 'iteration_*
 
 rm -fr ./tmp/export/
 genienlp export --path ./tmp/model -o ./tmp/export/
-test -f ./tmp/model/calibrator.pkl && cp ./tmp/model/calibrator.pkl ./tmp/export/
+tar -cJf ./model.tar.xz -C ./tmp/export .
 
-version=2
-for model_tag in org.thingpedia.models.contextual ; do
-	AWS_PROFILE=oval aws s3 sync ./tmp/export/ "s3://almond-training/staging/inference/${model_tag}:en-v${version}/"
-
-	set +x
-	ADMIN_TOKEN=$(secret-tool lookup almond.nlp_admin_token dev)
-	curl -v -d '' "https://nlp-staging.almond.stanford.edu/admin/reload/@${model_tag}/en?admin_token=${ADMIN_TOKEN}"
-	echo
-	set -x
-done
+AWS_PROFILE=oval aws s3 cp ./model.tar.xz "s3://thingpedia2/models/en-US/org.thingpedia.models.contextual/latest.tar.xz"
