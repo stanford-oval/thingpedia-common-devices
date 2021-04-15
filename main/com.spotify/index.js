1f19458678176b4916de0190fc6239fffffe1c74
Ryan Cheng <ryachen@nuevaschool.org>
2021-04-15 13:35:22 -0700

    fixed dataset generation error

--- main/com.spotify/index.js
+++ main/com.spotify/index.js
@@ -1032,7 +1032,7 @@ module.exports = class SpotifyDevice extends Tp.BaseDevice {
         playlist
     }) {
 
-        let playListURI = String(playlist);
+        let playListURI = await this.findPlaylist(playlist);
         playListURI = playListURI.substring(playListURI.indexOf("playlist:") + 9);
         let data = {
             "uris": [String(song)]
@@ -1044,10 +1044,9 @@ module.exports = class SpotifyDevice extends Tp.BaseDevice {
         playlist
     }) {
         const currentlyPlaying = await this.get_get_currently_playing();
-        if (currentlyPlaying.length === 0) {
+        if (currentlyPlaying.length === 0)
             throwError("no_song_error");
-        }
-        let playListURI = String(playlist);
+        let playListURI = await this.findPlaylist(playlist);
         playListURI = playListURI.substring(playListURI.indexOf("playlist:") + 9);
         let song = String(currentlyPlaying[0]["song"]);
         let data = {
@@ -1055,6 +1054,16 @@ module.exports = class SpotifyDevice extends Tp.BaseDevice {
         };
         return this.add_uris_to_playlist(playListURI, data);
 
+    }
+
+    async findPlaylist(name) {
+        const searchResults = await this.search(name, "playlist", 1);
+        if (!Object.prototype.hasOwnProperty.call(searchResults, 'playlists') || searchResults.playlists.total === 0) throwError('disallowed_action');
+        const playlist = searchResults.playlists.items[0];
+        if (playlist.owner.id == this.state.id) {
+            return playlist.uri;
+        }
+        throwError('disallowed_action');
     }
 
     async add_uris_to_playlist(playlistURL, uris) {
