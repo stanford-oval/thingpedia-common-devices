8aa514a7d9f3001f32f3052f00d4c7f77af0a283
arthbohra <arthbohra@gmail.com>
2021-04-24 19:36:58 -0700

    Editing my First Draft of goal_smart
    I changed up my code to fix the suggestions made by Dr. Campagna.

--- staging/com.scoreSmart/device.js
+++ staging/com.scoreSmart/device.js
@@ -3,33 +3,26 @@
 const Tp = require('thingpedia');
 const unirest = require("unirest");
 
-module.exports = class ScoreDevice extends Tp.BaseDevice {
+module.exports = class GoalDevice extends Tp.BaseDevice {
   constructor(engine, state) {
     super(engine, state);
-    this.uniqueId = 'com.scoreSmart';
-    this.name = 'ScoreSmart';
+    this.uniqueId = 'com.goal_smart';
+    this.name = 'GoalSmart';
     this.description = 'Keep up with the latest in soccer';
   }
 
   // getting league standings, given league id
   get_standings({ league_id }) {
-    const request = require('request');
-
-    const options = {
-      method: 'GET',
-      url: 'https://api-football-v1.p.rapidapi.com/v3/standings',
-      qs: { season: '2020', league: league_id },
-      headers: {
-        'x-rapidapi-key': 'b9b9b1e851msh45c99175d8f8df8p103e18jsn903a05143134',
+    return Tp.Helpers.Http.get('https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=' + league_id, {
+      extraHeaders: {
+        'x-rapidapi-key': this.constructor.metadata.auth.api_key,
         'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
         useQueryString: true
-      }
-    };
-
-    request(options, function (error, r, body) {
-      if (error) throw new Error(error);
-      const a = JSON.parse(body);
-      const b = a.response[0].league.standings[0];
+      },
+      accept: 'application/json'
+    }).then((tempResponse) => {
+      const a = JSON.parse(tempResponse);
+      const b = a.response;
       return b.map((obj) => {
         const r = obj.rank;
         const n = obj.team.name;
@@ -37,30 +30,27 @@ module.exports = class ScoreDevice extends Tp.BaseDevice {
         const p = obj.points;
         console.log(r + ". " + n + " | Form " + f + " | Points: " + p);
         return ({
-          Team: n,
-          Rank: r,
-          Form: f,
-          Points: p
+          team: n,
+          rank: r,
+          form: f,
+          points: p
         });
       });
+
     });
   }
   get_teamFixtures({ team_id }) {
-    const request = require('request');
 
-    const options = {
-      method: 'GET',
-      url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
-      qs: { team: team_id, next: '5' },
-      headers: {
-        'x-rapidapi-key': 'b9b9b1e851msh45c99175d8f8df8p103e18jsn903a05143134',
+    return Tp.Helpers.Http.get('https://api-football-v1.p.rapidapi.com/v3/fixtures/' + team_id, {
+
+      extraHeaders: {
+        'x-rapidapi-key': this.constructor.metadata.auth.api_key,
         'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
         useQueryString: true
-      }
-    };
-    request(options, function (error, r, body) {
-      if (error) throw new Error(error);
-      const a = JSON.parse(body);
+      },
+      accept: 'application/json'
+    }).then((tempResponse) => {
+      const a = JSON.parse(tempResponse);
       const b = a.response;
       return b.map((obj) => {
         const l = obj.league.name;
@@ -68,10 +58,10 @@ module.exports = class ScoreDevice extends Tp.BaseDevice {
         const n2 = obj.teams.away.name;
         const v = obj.fixture.venue.name;
         return [{
-          League: l,
-          Team1: n1,
-          Team2: n2,
-          Venue: v
+          league: l,
+          team1: n1,
+          team2: n2,
+          venue: v
         }]
 
       })
