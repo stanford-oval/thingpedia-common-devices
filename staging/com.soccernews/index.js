c8352801fd28b6735afc32ef46cd36b94b8ce0e1
arthbohra <arthbohra@gmail.com>
2021-04-26 13:08:56 -0700

    Adding Soccer News Tool
    I have created a new skill, in addition to my skill about soccer scores, that tracks soccer news

---
+++ staging/com.soccernews/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const Tp = require('thingpedia');
+const unirest = require("unirest");
+
+module.exports = class NewsDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+        super(engine, state);
+        this.uniqueId = 'com.soccer_news';
+        this.name = 'SoccerNews';
+        this.description = 'Keep up with the latest in soccer';
+    }
+
+    get_standings() {
+        return Tp.Helpers.Http.get('https://real-time-football-content.p.rapidapi.com/football?langs=en&limit=10&skip=0', {
+            extraHeaders: {
+                'x-rapidapi-key': this.constructor.metadata.auth.api_key,
+                'x-rapidapi-host': 'real-time-football-content.p.rapidapi.com',
+                useQueryString: true
+            },
+            accept: 'application/json'
+        }).then((tempResponse) => {
+            const a = JSON.parse(tempResponse);
+            return a.map((obj) => {
+                const t = obj.title;
+                const u = obj.url;
+                const d = obj.description;
+                const s = obj.source;
+
+                return ({
+                    title: t,
+                    summary: d,
+                    source: s,
+                    url: u
+                });
+            });
+
+        });
+    }
+}
+
