0d70830281e50e2815b7f43e4b153e54f97a7b4f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-06-11 15:55:00 -0700

    travis: try getting python 3.8

--- travis/start-home-assistant.sh
+++ travis/start-home-assistant.sh
@@ -3,9 +3,16 @@
 set -e
 set -x
 
-./scripts/run-home-assistant.sh &
+pyenv global 3.8.1
 
-# wait 60 seconds for Home Assistant to install itself and set up
-sleep 60
+mkdir -p ./tmp
+test -d ./tmp/homeassistant-venv || virtualenv ./tmp/homeassistant-venv
+. ./tmp/homeassistant-venv/bin/activate
+pip3 install 'homeassistant==2021.6.3'
+deactivate
+
+./scripts/run-home-assistant.sh &
+# wait 30 seconds for Home Assistant to install itself and set up
+sleep 30
 
 ./scripts/setup-ha-virtual-devices.js main universe
