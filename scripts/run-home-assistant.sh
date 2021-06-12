50a937272887ecfaed56e728a0c3f52e9dd12e96
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-06-11 18:05:45 -0700

    try a different way to install home assistant in travis

--- scripts/run-home-assistant.sh
+++ scripts/run-home-assistant.sh
@@ -8,11 +8,7 @@ shift $n
 set -e
 set -x
 
-mkdir -p $(dirname "${venv}")
-test -d "${venv}" || virtualenv "${venv}"
 . "${venv}"/bin/activate
-pip3 install 'homeassistant==2021.6.3'
-
 mkdir -p $(dirname "${config}")
 test -d "${config}" || cp -r "${srcdir}/../test/data/homeassistant" "${config}"
 exec python3 -m homeassistant -c "${config}"
--- travis/start-home-assistant.sh
+++ travis/start-home-assistant.sh
@@ -6,9 +6,13 @@ set -x
 pyenv global 3.8.1
 
 mkdir -p ./tmp
+
+git clone --depth 1 -b 2021.6.3 'https://github.com/home-assistant/core' ./tmp/homeassistant
+
 test -d ./tmp/homeassistant-venv || virtualenv ./tmp/homeassistant-venv
 . ./tmp/homeassistant-venv/bin/activate
-pip3 install 'homeassistant==2021.6.3'
+pip3 install -r ./tmp/homeassistant/requirements.txt
+pip3 install -e ./tmp/homeassistant
 deactivate
 
 ./scripts/run-home-assistant.sh &
