5837967bc390cc949814433e6581be8e1ca74f4a
Jake Wu <jmhw0123@gmail.com>
2021-06-30 00:46:55 +0800

    add _get_tunein_reponse method to the device.js

--- main/com.tunein/device.js
+++ main/com.tunein/device.js
@@ -16,5 +16,31 @@ module.exports = class TuneinRadioDevice extends Tp.BaseDevice {
          this.description = 'Stream radio through TuneIn API';
     }
 
+    _get_tunein_response(req) {
+        return new Promise( (resolve, reject) => {
+            axios.get(req.url, req)
+                .then(response => { 
+                    if (response.data.head.status == HTTP_STATUS_OK) {
+                        let content = response.data.body;
+                        if ('c' in req.params && BROWSE_ITEMS.includes(req.params.c.toLowerCase())) {
+                            return resolve(content);
+                        } else if (content.find(item => item.text.toLowerCase() == 'stations')) {
+                            return resolve(
+                                content.filter(item => item.text.toLowerCase() == 'stations')
+                                       .flatMap(({children}) => children)
+                                       .filter(channel => (channel.type.toLowerCase() == 'audio' && channel.item.toLowerCase() == 'station'))
+                            );
+                        } else {
+                            return resolve(content.filter(channel => (channel.type.toLowerCase() == 'audio' && channel.item.toLowerCase() == 'station')));
+                        }; 
+                    } else {
+                        reject(response.data);
+                        return new Error(`HTTP request error: ${response.data.head.fault}`);
+                    }
+                })
+                .catch(err => throwError("streaming_service_unaccessible"));
+        }).catch(err => throwError("streaming_service_unaccessible"));
+    }
+
     
 };
