be98b9f755ec105fb5199378754df68e9e0549ab
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-07-15 15:14:27 -0700

    Allow synthesizing a mix of simple and complex commands
    To bias the training data towards simple commands, do two separate
    generations, with different sets of hyperparameters.
    
    "Simple" generation is optimized for breadth: high pruning size
    so we don't prune out commands, low depth and number of turns,
    and compound commands disabled.
    
    "Complex" generation is as before optimized for depth and complexity:
    relatively low pruning size but high depth and number of turns.

--- travis/test-dataset.sh
+++ travis/test-dataset.sh
@@ -6,4 +6,4 @@ set -o pipefail
 
 # Test that we can generate a (small) dataset for the main + universe devices
 
-make release=universe subdatasets=1 target_pruning_size=20 max_turns=2 debug_level=2 parallel=1 datadir
+make release=universe subdatasets=1 simple_subdatasets=0 target_pruning_size=20 max_turns=2 debug_level=2 parallel=1 datadir
