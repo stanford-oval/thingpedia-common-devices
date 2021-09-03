27a561019037a3bb5fb7850e64f8a8fe380130ea
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-09-03 14:00:07 -0700

    Add a script to upload everything to local Thingpedia for demos

---
+++ scripts/upload-demo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -x
+
+./scripts/upload-all.sh \
+    builtin/org.thingpedia.dialogue-handler \
+    main/com.bing \
+    main/com.icanhazdadjoke \
+    main/com.smartnews \
+    main/com.yelp \
+    main/org.thingpedia.media-source \
+    main/org.thingpedia.media-player \
+    main/com.spotify \
+    main/org.thingpedia.iot.switch \
+    main/org.thingpedia.iot.* \
+    main/io.home-assistant
+
+mkdir -p tmp/entities
+npx genie download-entity-values \
+    --thingpedia-url https://thingpedia.stanford.edu/thingpedia \
+    -d ./tmp/entities \
+    --type com.yelp:restaurant_cuisine
+npx genie upload-entity-values \
+    --json ./tmp/entities/com.yelp:restaurant_cuisine.json \
+    --entity-id com.yelp:restaurant_cuisine \
+    --entity-name 'Cuisines in Yelp'
