8b3987caa1ddcb3f95467772d2e89dfbb190a5db
mehrad <mehrad@stanford.edu>
2022-01-31 16:05:27 -0800

    test-dataset: update translation argument

--- travis/test-dataset.sh
+++ travis/test-dataset.sh
@@ -9,5 +9,5 @@ make release=universe subdatasets=1 simple_subdatasets=0 target_pruning_size=20 
 
 # Translate that dataset to German
 make source=user all_names="eval" release=universe requote_skip_errors=true process_data
-make source=user all_names="eval" release=universe translation_model=Helsinki-NLP/opus-mt-en-de src_lang=en tgt_lang=de nmt_model=nmt val_batch_size=200 temperature=0.0 translate_data
+make source=user all_names="eval" release=universe model_name_or_path=Helsinki-NLP/opus-mt-en-de src_lang=en tgt_lang=de nmt_model=nmt val_batch_size=200 temperature=0.0 translate_data
 make source=user all_names="eval" release=universe src_lang=en tgt_lang=de nmt_model=nmt requote_skip_errors=true augment_override_flags= postprocess_data
