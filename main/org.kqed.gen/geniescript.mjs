bf25dd4730fed30a719bd8d45d87678098074a84
Jackie Yang <jackie@jackieyang.me>
2022-03-27 18:32:01 -0700

    Added kqed.gen

---
+++ main/org.kqed.gen/geniescript.mjs
@@ -0,0 +1,104 @@
+"use strict";
+
+
+const Tp = require("thingpedia");
+
+export class GeniescriptDlg {
+    constructor(user_target, skill_name) {
+        this._user_target = user_target;
+        this._skill_name = skill_name;
+        this._last_result = null;
+        this._last_branch = null;
+        this._last_analyzed = null;
+        this._last_messages = [];
+        this._last_expecting = null;
+        this._last_target = null;
+    }
+
+    async *expect(func_map) {
+        if (this._last_analyzed !== null) {
+            this._last_result = {
+                messages: this._last_messages,
+                expecting: this._last_expecting,
+                context: this._last_analyzed,
+                agent_target: this._last_target
+            };
+            this._last_messages = [];
+            this._last_expecting = null;
+            this._last_target = null;
+            this._last_analyzed = null;
+        }
+        while (true) {
+            let input = yield this._last_result;
+            if (input.type === "utterance") {
+                for (let func_key of func_map.keys()) {
+                    let regExp = new RegExp(func_key, 'i');
+                    let match = regExp.exec(input.content);
+                    if (match) {
+                        this._last_branch = func_key;
+                        this._last_result = {
+                            confident: Tp.DialogueHandler.Confidence.EXACT_IN_DOMAIN_COMMAND,
+                            utterance: input.content,
+                            user_target: this._user_target
+                        };
+                        break;
+                    }
+                }
+                this._last_result = {
+                    confident: Tp.DialogueHandler.Confidence.OUT_OF_DOMAIN_COMMAND,
+                    utterance: input.content,
+                    user_target: ''
+                };
+            } else if (input.type === "analyzed") {
+                this._last_analyzed = input.content;
+                let current_func = func_map.get(this._last_branch);
+                if (current_func.constructor.name === "GeneratorFunction") {
+                    yield * current_func();
+                } else {
+                    await current_func();
+                }
+                break;
+            }
+        }
+    }
+
+    // TODO: say something in the middle of the process
+    say(messages, target=null, expecting = null) {
+        if (target === null) target = this._skill_name + ".reply";
+        this._last_messages = this._last_messages.concat(messages);
+        this._last_target = target;
+        this._last_expecting = expecting;
+    }
+}
+
+export class AbstractGeniescriptHandler {
+    constructor() {
+        if (this.constructor === AbstractGeniescriptHandler) {
+            throw new Error("Abstract classes can't be instantiated.")
+        }
+    }
+
+    // TODO: Implementation serialization
+    // getState() {
+    //     return {_GenieScriptDlgLogic: logic.serialize}
+    // }
+
+    async initialize() {
+        this._logic = this.logic();
+        await this._logic.next();
+    }
+
+    async analyzeCommand(utterance) {
+        return this._logic.next({ type: "utterance", content: utterance }).value;
+    }
+
+    async getReply(analyzed) {
+        return this._logic.next({ type: "analyzed", content: analyzed }).value;
+    }
+
+    async *logic() {
+    }
+
+}
+
+
---
+++ main/org.kqed.gen/index.mjs
@@ -0,0 +1,282 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of org.kqed
+//
+// Copyright 2022 undefined <undefined>
+//
+// See LICENSE for details
+"use strict";
+
+import {GeniescriptDlg, AbstractGeniescriptHandler} from "./geniescript.mjs"
+
+const Tp = require('thingpedia');
+const TT = require('thingtalk');
+const interpolate = require('string-interp');
+
+const RSS_URL = "https://www.omnycontent.com/d/playlist/0af137ef-751e-4b19-a055-aaef00d2d578/87fdd794-f90e-4280-920f-ab89016e8062/d72d17c7-e1c8-4763-98eb-ab89016ed36a/podcast.rss";
+
+const DEVICE_ERROR = {
+    no_postcasts_available: 'no_podcasts_available',
+    unsupported_version: 'unsupported_version',
+    service_unavailable: 'service_unavailable'
+};
+
+class KqedDialogueHandler extends AbstractGeniescriptHandler{
+    /**
+     *
+     * @param {string} locale
+     * @param {string} timezone
+     */
+    constructor(locale, timezone) {
+        super();
+        this._locale = locale;
+        this._timezone = timezone;
+        this._ = KqedDevice.gettext.gettext;
+        this._item = 0;
+        this._podcasts = [];
+        this._askedResume = false;
+
+        let user_target = '$dialogue @org.thingpedia.dialogue.transaction.execute;\n' +
+            '@org.kqed_gen.kqed_podcasts();';
+        this.dlg = GeniescriptDlg(user_target);
+    }
+
+    _interp(string, args) {
+        return interpolate(string, args, {
+            locale: this._locale,
+            timezone: this._timezone,
+        });
+    }
+
+    get priority() {
+        return Tp.DialogueHandler.Priority.PRIMARY;
+    }
+
+    get icon() {
+        return 'org.kqed';
+    }
+
+    getState() {
+        return { lastQuerySuggestion: this._lastQuerySuggestion };
+    }
+
+    async initialize(initialState) {
+        if (initialState)
+            this._lastQuerySuggestion = initialState.lastQuerySuggestion;
+        this._podcasts = await preFetchItems();
+        return null;
+    }
+
+    reset() {
+        this._lastQuerySuggestion = null;
+        this._item = 0;
+        this._podcasts = [];
+        this._askedResume = false;
+    }
+
+    * yes_no(yes_action, no_action) {
+        let self = this;
+        self.dlg.expect(new Map(Object.entries({
+            "\\b(yes|yeah|yep|sure|go ahead)\\b": async function () {
+                yes_action();
+            },
+            "\\b(no|nah|nope)\\b": async function () {
+                no_action();
+            }
+        })))
+    }
+
+    play() {
+        this.dlg.say([
+            self._interp(self._("Play KQED now."), {}),
+            {
+                type: 'rdl',
+                webCallback: `${self._podcasts[self._item].link}::${self._item}`,
+                displayTitle: `${self._podcasts[self._item].title}`,
+            },
+            {
+                type: 'text',
+                text: `${self._podcasts[self._item].description}`
+            }
+        ]);
+    }
+
+    stop() {
+        self.dlg.say(
+            [self._interp(self._("Stop playing."), {})]
+        )
+    }
+
+    next() {
+        self.dlg.say([
+            this._interp(this._("Play next."), {}),
+            {
+                type: 'rdl',
+                webCallback: `${this._podcasts[this._item].link}::${this._item}`,
+                displayTitle: `${this._podcasts[this._item].title}`,
+            },
+            {
+                type: 'text',
+                text: `${this._podcasts[this._item].description}`
+            },
+            // TODO: call do_kqed_play
+            this._interp(this._("Play next?"), {}),
+        ])
+    }
+
+    resume() {
+        this.dlg.say([
+            self._interp(this._("resume playing?"), {})
+        ]);
+    }
+
+    async *logic() {
+        let self = this;
+        while (true) {
+            self.dlg.expect(new Map(Object.entries({
+                "^\\s*kqed now\\b": (async function * () {
+                    if (self._item) {
+                        resume();
+                        yield * self.yes_no(
+                            function () {
+                                self.play();
+                            },
+                            function () {
+                                self.stop();
+                            }
+                        )
+                    } else {
+                        self.play()
+                        self.dlg.say([
+                            // TODO: call do_kqed_play
+                            self._interp(self._("Play next?"), {}),
+                        ]);
+                        self.yes_no(function () {
+                            self.next()
+                        }, function () {
+                            self.stop()
+                        })
+                    }
+                }),
+            })));
+        }
+    }
+}
+
+async function preFetchItems() {
+    const blob = await Tp.Helpers.Http.get(RSS_URL).then(
+        (result) => Tp.Helpers.Xml.parseString(result)
+    ).then(
+        (parsed) => {
+            const podcasts = parsed.rss.channel[0];
+            if (podcasts.item === undefined)
+                throw new Error(DEVICE_ERROR.no_postcast_available);
+            return podcasts.item.map((item) => {
+                return ({
+                    id: new Tp.Value.Entity(item.guid[0]['_'], null),
+                    title: item.title[0],
+                    description: item.description[0],
+                    link: item['media:content'][0]['$'].url,
+                    // link: item.enclosure[0]['$'].url,
+                    date: new Date(item.pubDate[0]),
+                    duration: Number(item['itunes:duration'][0])
+                });
+            });
+        }).catch((err) => {
+        if (err.code === 404)
+            throw new Error("Invalid URL");
+        else
+            throw new Error(DEVICE_ERROR.service_unavailable);
+    });
+    return blob.slice(0 ,3);
+}
+
+async function* fetchItems() {
+    const items = await Tp.Helpers.Http.get(RSS_URL).then(
+        (result) => Tp.Helpers.Xml.parseString(result)
+    ).then(
+        (parsed) => {
+            const podcasts = parsed.rss.channel[0];
+            if (podcasts.item === undefined)
+                throw new Error(DEVICE_ERROR.no_postcast_available);
+            return podcasts.item.map((item) => {
+                return ({
+                    id: new Tp.Value.Entity(item.guid[0]['_'], null),
+                    title: item.title[0],
+                    description: item.description[0],
+                    link: item['media:content'][0]['$'].url,
+                    // link: item.enclosure[0]['$'].url,
+                    date: new Date(item.pubDate[0]),
+                    duration: Number(item['itunes:duration'][0])
+                });
+            });
+        }).catch((err) => {
+        if (err.code === 404)
+            throw new Error("Invalid URL");
+        else
+            throw new Error(DEVICE_ERROR.service_unavailable);
+    });
+    for (const item of items)
+        yield item;
+}
+
+class KqedDevice extends Tp.BaseDevice {
+    constructor(engine, state) {
+        super(engine, state);
+        this.uniqueId = 'org.kqed';
+        this.name = "KQED Now";
+        this.description = "A daily News podcast from KQED";
+        this._dialogueHandler = new KqedDialogueHandler(this.platform.locale, this.platform.timezone);
+    }
+    
+    queryInterface(iface) {
+        switch (iface) {
+        case 'dialogue-handler':
+            return this._dialogueHandler;
+
+        default:
+            return super.queryInterface(iface);
+        }
+    }
+    
+    async *get_kqed_podcasts() {
+        try {
+            yield* fetchItems();
+        } catch(error) {
+            throw new Error(DEVICE_ERROR.service_unavailable);
+        }
+    }
+
+    pprint() {
+        console.log("here");
+        return "kqed";
+    }
+
+    async do_kqed_play(env, playable_link) {
+        const engine = this.engine;
+        if (engine.audio && engine.audio.playURLs) {
+            if (!await engine.audio.checkCustomPlayer({ type: 'url' }, env.conversation))
+                throw new Error(DEVICE_ERROR.unsupported_version);
+            // play asynchronously: playing will call requestAudio on the audio controller,
+            // which will wait until the agent is done speaking, so we must return here
+            engine.audio.playURLs(this, [playable_link], env.conversation).catch((e) => {
+                console.error(`Failed to play radio URL`, e);
+            });
+            return;
+        }
+
+        const audio_player = this.platform.getCapability('audio-player');
+        if (!audio_player) {
+            throw new Error(DEVICE_ERROR.unsupported_version);
+        } else {
+            // const playable_link = this._http_post(url);
+            try {
+                audio_player.play(playable_link);
+                return;
+            } catch(e) {
+                throw new Error(DEVICE_ERROR.service_unavailable);
+            }
+        }
+    }
+}
+module.exports = KqedDevice;
