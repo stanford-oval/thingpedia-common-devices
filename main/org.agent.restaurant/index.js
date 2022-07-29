bbd009c33db5cf3b047227c087e80c54ca5a75ed
Jake Wu <jmhw0123@gmail.com>
2022-07-29 12:07:46 +0800

    formalize simple example

--- main/org.agent.restaurant/index.js
+++ main/org.agent.restaurant/index.js
@@ -22,7 +22,9 @@ class RestaurantAgentDialogueGenHandler extends Genie.DialogueAgent.Geniescript.
         this._locale = locale;
         this._timezone = timezone;
         this._ = RestaurantAgent.gettext.gettext;
-        this._reply = "I'm your restaurant booking helper. would you like to find a restaurant?";
+        this._introMsg = "Hello there! I'm your restaurant booking helper. How may I help you?\n" + 
+        "You can say things like 'find me a restaurant', " +
+        "'I want chinese food' or 'give me a good restaurant nearby'.";
     }
 
     _interp(string, args) {
@@ -36,115 +38,27 @@ class RestaurantAgentDialogueGenHandler extends Genie.DialogueAgent.Geniescript.
         return { lastQuerySuggestion: this._lastQuerySuggestion };
     }
 
-    _greet(msg) {
-        return {
-            messages: [this._(msg)],
-            context: 'null',
-            agent_target: '$dialogue @org.thingpedia.dialogue.transaction.greet;',
-            expecting: null,
-        };
-    }
-
-    async initialize(initialState, showWelcome) {
+    async initialize(initialState) {
         await super.initialize();
         if (initialState)
             this._lastQuerySuggestion = initialState.lastQuerySuggestion;
-        const Msg = "\n\nHello there! I'm your restaurant agent. How may I help you?" + 
-                    "\nYou can say things like 'find me a restaurant' or " +
-                    "\n'I want chinese food' or 'give me a good restaurant nearby'.";
-        if (showWelcome)
-            return this._greet(Msg);
-        else
-            return null;
+        return null;
     }
 
     reset() {
         this._lastQuerySuggestion = null;
     }
 
-    async *yes_no(prompt) {
-        let self = this;
-        return yield * self.dlg.expect(new Map(Object.entries({
-            "\\b(yes|yeah|yep|sure|go ahead)\\b": async function() {
-                return true;
-            },
-            "\\b(no|nah|nope)\\b": async function() {
-                return false;
-            }
-        })), prompt);
-    }
-
-    _generate_followup(reply) {
-        if (reply.raw_results && Object.keys(reply.raw_results).length) {
-            const [appCall, blob] = reply.raw_results[0];
-            const [appName, funcName] = appCall.split(":");
-            console.log(blob);
-            if (appName.includes("yelp")) {
-                switch (funcName) {
-                    case "restaurant": {
-                        if (blob.review_count > 500 && blob.rating >= 3)
-                            this._reply = "They seem very popular, which one would you like to book?";
-                        else if (blob.geo)
-                            this._reply = "Would you like to book your Uber ride to get there?";
-                        else
-                            this._reply = "Which restaurant would you like to book?";
-                        break;
-                    }
-                    default: {
-                        this._reply = "I'm your restaurant booking helper. Would you like to find a restaurant?";
-                        break;
-                    }
-                }
-            } else if (appName.includes("weather")) {
-                switch (funcName) {
-                    case "current": {
-                        this._reply = "Would you like to find a restaurant there?";
-                        break;
-                    }
-                    default: {
-                        this._reply = "I'm your restaurant booking helper. Would you like to find a restaurant?";
-                        break;
-                    }
-                }
-            } else {
-                return false;
-            }
-            return true;
-        } else 
-            return false;
-    }
-
     async *logic() {
         let self = this;
-        yield * self.dlg.expect(new Map(Object.entries({
-
-        })), 
-        new Map([
-            [
-                (reply) => reply === null,
-                (
-                    async function*() {
-                        self.dlg.say(self._reply);
-                    }
-                )
-            ],
-            [
-                (reply) => this._generate_followup(reply),
-                (
-                    async function*() {
-                        self.dlg.say(self._reply);
-                    }
-                )
-            ],
-            [
-                (reply) => true,
-                (
-                    async function*() {
-                        self.dlg.say(self._reply);
-                    }
-                )
-            ],
-        ]));
+        self.dlg.say(this._introMsg);
+        while (true)
+            yield * self.dlg.expect(
+                new Map([]),
+                Genie.ThingTalkUtils.isKind('yelp', 'restaurant'),
+                (reply) => reply,
+                "I'm your restaurant booking helper. Would you like to find a restaurant?"
+            );
     }
 }
 
